2ed5133a80f133abfda890419afca0320e80b98f
cto <cto@adve.live>
2023-08-10 23:53:55 +0300

    3rd

--- Task 2/Task_2.py
+++ Task 2/Task_2.py
@@ -3,26 +3,26 @@ import matplotlib.pyplot as plt
 
 # Semi-major axes in AU
 semi_major_axes = {
-    "Mercury": 0.39,
-    "Venus": 0.72,
-    "Earth": 1.0,
-    "Mars": 1.52, 
-    #"Jupiter": 5.2,
-    #"Saturn": 9.58,
-    #"Uranus": 19.22,
-    #"Neptune": 30.05
+    #"Mercury": 0.39,
+    #"Venus": 0.72,
+    #"Earth": 1.0,
+    #"Mars": 1.52, 
+    "Jupiter": 5.2,
+    "Saturn": 9.58,
+    "Uranus": 19.22,
+    "Neptune": 30.05
 }
 
 # Eccentricities
 eccentricities = {
-    "Mercury": 0.205,
-    "Venus": 0.007,
-    "Earth": 0.017,
-    "Mars": 0.094,
-    #"Jupiter": 0.049,
-    #"Saturn": 0.056,
-    #"Uranus": 0.046,
-    #"Neptune": 0.010
+    #"Mercury": 0.205,
+    #"Venus": 0.007,
+    #"Earth": 0.017,
+    #"Mars": 0.094,
+    "Jupiter": 0.049,
+    "Saturn": 0.056,
+    "Uranus": 0.046,
+    "Neptune": 0.010
 }
 
 # Angle values
@@ -40,11 +40,16 @@ for planet in semi_major_axes:
     y = r * np.sin(theta)
     ax.plot(x, y, label=planet)
 
+
+# Plot the yellow dot at (0, 0)
+ax.plot(0, 0, 'yo', label='Sun')
+
+
 # Set equal aspect ratio
 ax.set_aspect('equal', adjustable='box')
 
 # Add labels and legend
-ax.set_title("Inner Planet Orbits")
+ax.set_title("Outer Planet Orbits")
 ax.set_xlabel("X (AU)")
 ax.set_ylabel("Y (AU)")
 ax.legend()

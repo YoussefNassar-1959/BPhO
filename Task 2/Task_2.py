a4c668973bc8436675e373f6271f7abde087dcf8
cto <cto@adve.live>
2023-08-10 23:45:44 +0300

    dkfds

--- Task 2/Task_2.py
+++ Task 2/Task_2.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Semi-major axes (in arbitrary units)
+# Semi-major axes in AU
 semi_major_axes = {
     "Mercury": 0.39,
     "Venus": 0.72,
     "Earth": 1.0,
-    "Mars": 1.52,
-    "Jupiter": 5.2,
-    "Saturn": 9.58,
-    "Uranus": 19.22,
-    "Neptune": 30.05
+    "Mars": 1.52, 
+    #"Jupiter": 5.2,
+    #"Saturn": 9.58,
+    #"Uranus": 19.22,
+    #"Neptune": 30.05
 }
 
 # Eccentricities
@@ -19,10 +19,10 @@ eccentricities = {
     "Venus": 0.007,
     "Earth": 0.017,
     "Mars": 0.094,
-    "Jupiter": 0.049,
-    "Saturn": 0.056,
-    "Uranus": 0.046,
-    "Neptune": 0.010
+    #"Jupiter": 0.049,
+    #"Saturn": 0.056,
+    #"Uranus": 0.046,
+    #"Neptune": 0.010
 }
 
 # Angle values
@@ -31,7 +31,7 @@ theta = np.linspace(0, 2 * np.pi, 1000)
 # Create a figure and axis
 fig, ax = plt.subplots(figsize=(8, 8))
 
-# Plot planet orbits
+# Plot inner planet orbits
 for planet in semi_major_axes:
     a = semi_major_axes[planet]
     e = eccentricities[planet]
@@ -44,11 +44,10 @@ for planet in semi_major_axes:
 ax.set_aspect('equal', adjustable='box')
 
 # Add labels and legend
-ax.set_title("Planet Orbits")
-ax.set_xlabel("X (Arbitrary Units)")
-ax.set_ylabel("Y (Arbitrary Units)")
+ax.set_title("Inner Planet Orbits")
+ax.set_xlabel("X (AU)")
+ax.set_ylabel("Y (AU)")
 ax.legend()
 
 # Show the plot
 plt.show()
-

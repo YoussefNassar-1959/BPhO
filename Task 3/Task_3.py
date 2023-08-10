549ecea9a86bc69e9ca3f39ae18d5e7562ccd0fc
cto <cto@adve.live>
2023-08-11 02:11:01 +0300

    planetary system simulation

--- Task 3/Task_3.py
+++ Task 3/Task_3.py
@@ -2,7 +2,7 @@ import pygame
 import math
 pygame.init()
 
-WIDTH, HEIGHT =  800, 800
+WIDTH, HEIGHT =  1000, 1000
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
@@ -97,6 +97,9 @@ def main():
 	earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
 	earth.y_vel = 29.783 * 1000 
 
+	jupiter = Planet(5.2 * Planet.AU, 0, 20, (255, 165, 0), 1_898.13 * 10**24)
+	jupiter.y_vel = 13.06 * 1000
+	
 	mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
 	mars.y_vel = 24.077 * 1000
 
@@ -106,7 +109,7 @@ def main():
 	venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
 	venus.y_vel = -35.02 * 1000
 
-	planets = [sun, earth, mars, mercury, venus]
+	planets = [sun, earth, mars, mercury, venus, jupiter]
 
 	while run:
 		clock.tick(60)

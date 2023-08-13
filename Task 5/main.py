f3dc11164eee989a1c3fea5971ffac7316f801f5
HazemMoghni <80206463+HazemMoghni@users.noreply.github.com>
2023-08-13 15:50:48 +0300

    Update main.py

--- Task 5/main.py
+++ Task 5/main.py
@@ -1,47 +1,56 @@
 import math
 import numpy as np
 
-P_Pluto=248.348
-e_Pluto=0.25
-theta_i=0
-theta_f=2*math.pi
-t_i=0
-t_f=P_Pluto
-N=1000
-Pluto_time_constant=theta_constant(P_Pluto,e_Pluto)
-theta_values = np.linspace(theta_i, theta_f, N)
-time_values = np.linespace(t_i,t_f,N)
-integrand_values = integrand(theta_values)
-cumulative_sum_values = simpson(integrand, theta_i, theta_f, N)
+P_Pluto = 248.348
+e_Pluto = 0.25
+theta_i = 0
+theta_f = 2 * math.pi
+t_i = 0
+t_f = P_Pluto
+N = 1000
 
-def time_constant(P,e):
-    time_constant=(P*(1-e**2)**1.5)/(2*math.pi)
+def time_constant(P, e):
+    time_constant = (P * (1 - e ** 2) ** 1.5) / (2 * math.pi)
     return time_constant
 
-def theta_2_time_integrand(e,theta):
-    time_integrand=(1-e*math.cos(theta))**(-2)
+def theta_2_time_integrand(e, theta):
+    time_integrand = (1 - e * math.cos(theta)) ** (-2)
     return time_integrand
 
-# def time_2_theta(constant,P,e,t):
-#    theta=-constant(P,e)/(4*y)+0.5
-#    return theta
-
-def simpson(integrand,a,b,n):
+def simpson(integrand, a, b, n):
     # calculating step size
     h = (b - a) / n
     
-    # Finding sum 
-    int = integrand(a) + integrand(b)
+    # Finding sum
+    integral_sum = integrand(a) + integrand(b)
     
-    for i in range(1,n):
-        k = a + i*h
+    for i in range(1, n):
+        k = a + i * h
         
-        if i%2 == 0:
-            int += 2 * integrand(k)
+        if i % 2 == 0:
+            integral_sum += 2 * integrand(k)
         else:
-            int += 4 * integrand(k)
+            integral_sum += 4 * integrand(k)
     
     # Finding final integration value
-    int = int * h/3
+    integral_sum *= h / 3
 
-    return int
+    return integral_sum
+
+# Compute the time constant for Pluto
+Pluto_time_constant = time_constant(P_Pluto, e_Pluto)
+
+# Define the theta values
+theta_values = np.linspace(theta_i, theta_f, N)
+
+# Define the time values
+time_values = np.linspace(t_i, t_f, N)
+
+# Compute the integrand values using the theta_2_time_integrand function
+integrand_values = theta_2_time_integrand(e_Pluto, theta_values)
+
+# Compute the cumulative sum values using the simpson function
+cumulative_sum_values = simpson(theta_2_time_integrand, theta_i, theta_f, N)
+
+# Optional: If you want to convert the cumulative_sum_values to time units using the Pluto_time_constant
+cumulative_sum_time_values = cumulative_sum_values * Pluto_time_constant

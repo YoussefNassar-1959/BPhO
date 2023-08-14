7ea3ccbb27a1a72c8f06c33acf45b3901792b8a3
Youssef Nassar <youssef.1021135@stemegypt.edu.eg>
2023-08-14 03:56:14 +0300

    first actual 4

--- Task 4/simple_solar_system.py
+++ Task 4/simple_solar_system.py
@@ -9,15 +9,28 @@ sun = Sun(solar_system)
 planets = (
     Planet(
         solar_system,
-        position=(150, 50, 0),
-        velocity=(0, 5, 5),
+        mass=3.3011e23,  # Mass of Mercury in kg
+        position=(57.9, 0, 0),  # Semi-major axis of Mercury's orbit in million km
+        velocity=(0, 47.87, 0),  # Orbital velocity of Mercury in km/s
     ),
     Planet(
         solar_system,
-        mass=20,
-        position=(100, -50, 150),
-        velocity=(5, 0, 0)
-    )
+        mass=4.8675e24,  # Mass of Venus in kg
+        position=(108.2, 0, 0),  # Semi-major axis of Venus's orbit in million km
+        velocity=(0, 35.02, 0),  # Orbital velocity of Venus in km/s
+    ),
+    Planet(
+        solar_system,
+        mass=5.972e24,  # Mass of Earth in kg
+        position=(149.6, 0, 0),  # Semi-major axis of Earth's orbit in million km
+        velocity=(0, 29.78, 0),  # Orbital velocity of Earth in km/s
+    ),
+    Planet(
+        solar_system,
+        mass=6.39e23,  # Mass of Mars in kg
+        position=(227.9, 0, 0),  # Semi-major axis of Mars's orbit in million km
+        velocity=(0, 24.077, 0),  # Orbital velocity of Mars in km/s
+    ),
 )
 
 while True:

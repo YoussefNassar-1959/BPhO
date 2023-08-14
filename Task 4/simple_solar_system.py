ae1dcdf092e1aac5f9004dce27fa156c9325a2df
Youssef Nassar <youssef.1021135@stemegypt.edu.eg>
2023-08-14 15:44:56 +0300

    dk

--- Task 4/simple_solar_system.py
+++ Task 4/simple_solar_system.py
@@ -6,38 +6,19 @@ solar_system = SolarSystem(400, projection_2d=False)
 
 sun = Sun(solar_system)
 
-# Modify the planets tuple to include actual data for the first four planets
 planets = (
     Planet(
         solar_system,
-        mass=1.989e30,  # Mass of the Sun
-        position=(0, 0, 0),
-        velocity=(0, 0, 0),
+        mass=30,
+        position=(150, 50, 0),
+        velocity=(0, 5, 5),
     ),
     Planet(
         solar_system,
-        mass=0.33011e24,  # Mass of Mercury
-        position=(57.9e6, 0, 0),  # Distance from the Sun in meters
-        velocity=(0, 47.87e3, 0),  # Velocity in m/s
-    ),
-    Planet(
-        solar_system,
-        mass=4.8675e24,  # Mass of Venus
-        position=(108.2e6, 0, 0),
-        velocity=(0, 35.02e3, 0),
-    ),
-    Planet(
-        solar_system,
-        mass=5.972e24,  # Mass of Earth
-        position=(149.6e6, 0, 0),
-        velocity=(0, 29.78e3, 0),
-    ),
-    Planet(
-        solar_system,
-        mass=0.64171e24,  # Mass of Mars
-        position=(227.9e6, 0, 0),
-        velocity=(0, 24.077e3, 0),
-    ),
+        mass=20,
+        position=(100, -50, 150),
+        velocity=(5, 0, 0) 
+    )
 )
 
 while True:
